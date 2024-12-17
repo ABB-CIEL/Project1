5495465532f6a2f0db5afbae681b6fb9f8eeee21
thierry abbey <habbey480@gmail.com>
2024-12-17 08:53:08 +0100

    #7 creation de 4 carres

--- Project1/mcarre.cpp
+++ Project1/mcarre.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-    // Création de deux carrés
-    CCarre c1, c2;
+    // Création de quatre carrés
+    CCarre c1, c2, c3, c4;
 
     // Initialisation des caractéristiques de c1
     c1.Setsx(5);
@@ -22,6 +22,22 @@ int main() {
 
     cout << "\nCaractéristiques initiales de c2 :" << endl;
     c2.Afficher();
+
+    // Initialisation des caractéristiques de c3
+    c3.Setsx(25);
+    c3.Setsy(35);
+    c3.Setcote(40);
+
+    cout << "\nCaractéristiques initiales de c3 :" << endl;
+    c3.Afficher();
+
+    // Initialisation des caractéristiques de c4
+    c4.Setsx(35);
+    c4.Setsy(45);
+    c4.Setcote(50);
+
+    cout << "\nCaractéristiques initiales de c4 :" << endl;
+    c4.Afficher();
 
     // Test de la méthode Deplacer avec c1
     cout << "\nDeplacement de c1 vers le nord avec un saut de 3..." << endl;
@@ -42,6 +58,26 @@ int main() {
     cout << "\nDeplacement de c2 selon le vecteur (dx = -3, dy = 7)" << endl;
     c2.Deplacer(-3, 7);
     c2.Afficher();
+
+    // Test de la méthode Deplacer avec c3
+    cout << "\nDeplacement de c3 vers le sud avec un saut de 2..." << endl;
+    c3.Deplacer('s', 2);
+    c3.Afficher();
+
+    // Test de la méthode surchargée Deplacer avec un vecteur pour c3
+    cout << "\nDeplacement de c3 selon le vecteur (dx = 1, dy = 1)..." << endl;
+    c3.Deplacer(1, 1);
+    c3.Afficher();
+
+    // Test de la méthode Deplacer avec c4
+    cout << "\nDeplacement de c4 vers l'ouest avec un saut de 4..." << endl;
+    c4.Deplacer('o', 4);
+    c4.Afficher();
+
+    // Test de la méthode surchargée Deplacer avec un vecteur pour c4
+    cout << "\nDeplacement de c4 selon le vecteur (dx = -2, dy = -3)" << endl;
+    c4.Deplacer(-2, -3);
+    c4.Afficher();
 
     return 0;
 }
